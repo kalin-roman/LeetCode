4b5eeb0c5c73573dae319a88f5b1d1d627610f28
Roman Kalin <kalin.roman.m@gmail.com>
2023-04-12 21:53:22 +0300

    new_task

--- 58_Length_of_Last_Word.py
+++ 58_Length_of_Last_Word.py
@@ -1,12 +1,24 @@
-s = "luffy is still joyboy "
-word = ''
+s = "  luffy is stilljoyboy"
 
-for i,x in enumerate(s):
-    if i == len(s)-1 and x == ' ':
-        break
-    elif x != ' ':
-        word += x
-    else:
-        word = ''
 
-print(len(word))
+def f1(s):
+    word = ''
+    for i,x in enumerate(s):
+        if i == len(s)-1 and x == ' ' :
+            break
+        elif x != ' ':
+            word += x
+        elif s[i+1] != ' ':
+            word = ''
+
+def f2(s):
+    return len(set(s.split()).pop())
+
+# def f3(s):
+#     return 
+# print(f3(s))
+
+a = f1
+x = lambda s : len(s.split()[-1])
+print(x(s))
+
